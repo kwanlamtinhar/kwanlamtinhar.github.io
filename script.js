1c776748018df65d178194fb7f15f4a439733475
kwanlamtinhar <kwanlamtinhar@gmail.com>
2020-08-05 18:00:58 +0800

    0

--- script.js
+++ script.js
@@ -325,17 +325,11 @@ function change_content(theId,back){
 $(document).ready(function() {
     $('.twins').clone().appendTo('.content-wrapper');
 
-    var middle_height = $('.bottom').offset().top - $('.top').offset().top - $('.top').height();
-    $('.timeline-container-outer').css("--height",middle_height+'px');
-
     set_timeline();
-    $('.timeline').css("margin-top",middle_height*0.3+'px');
-    $('.timeline').css("margin-bottom",middle_height*0.6+'px');
+    
     $('.month-row').after('<img src="img/line.png" height="55px" width="auto" style="margin: 3px">');
 
     set_tag();
-
-    $('.content-wrapper').css("--opened-height" , $('.content-wrapper').height()+middle_height+'px');
 
     set_scroll();   
     // $('.green').css('bottom', $('.myBody').height() - $('.bottom').offset().top+'px');
@@ -350,6 +344,12 @@ $(document).ready(function() {
 
     add_to_preload();
     preload(imgAll);
+
+    var middle_height = $('.bottom').offset().top - $('.top').offset().top - $('.top').height();
+    $('.timeline-container-outer').css("height",middle_height+'px');
+    $('.timeline').css("margin-top",middle_height*0.3+'px');
+    $('.timeline').css("margin-bottom",middle_height*0.6+'px');
+    $('.content-wrapper').css("--opened-height" , $('.content-wrapper').height()+middle_height+'px');
 });
 
 $(window).on('load', function () {
